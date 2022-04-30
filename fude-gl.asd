52d45eb28430237b440e4ace979f88ffb2cf5101
SATO Shinichi <hyotang.karakoma@gmail.com>
2022-04-30 19:08:26 +0900

    Fix: *GLSL-FUNCTIONS* to use string rather than symbol.

--- fude-gl.asd
+++ fude-gl.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "fude-gl"
   :version
-  "2.28.7"
+  "2.28.8"
   :description
   "Fundamental Utility Definitions Especially for openGL."
   :license "MIT"
--- src/glsl.lisp
+++ src/glsl.lisp
@@ -2,60 +2,62 @@
 
 (defvar *glsl-functions*
   (uiop:list-to-hash-set
-    (mapcar #'symbol-name
-            '(;;;; Built in functions.
-              ;;;; https://www.khronos.org/registry/OpenGL-Refpages/gl4/index.php
-              abs acos acosh all any asin asinh atan atanh atomicadd
-              atomicand atomiccompswap atomiccounter atomiccounterdecrement
-              atomiccounterincrement atomicexchange atomicmax atomicmin
-              atomicor atomicxor barrier bitcount bitfieldextract
-              bitfieldinsert bitfieldreverse ceil clamp cos cosh cross degrees
-              determinant dfdx dfdxcoarse dfdxfine dfdy dfdycoarse dfdyfine
-              distance dot emitstreamvertex emitvertex endprimitive
-              endstreamprimitive equal exp exp2 faceforward findlsb findmsb
-              floatbitstoint floatbitstouint floor fma fract frexp fwidth
-              fwidthcoarse fwidthfine gl_clipdistance gl_culldistance
-              gl_fragcoord gl_fragdepth gl_frontfacing gl_globalinvocationid
-              gl_helperinvocation gl_instanceid gl_invocationid gl_layer
-              gl_localinvocationid gl_localinvocationindex gl_numsamples
-              gl_numworkgroups gl_patchverticesin gl_pointcoord gl_pointsize
-              gl_position gl_primitiveid gl_primitiveidin gl_sampleid
-              gl_samplemask gl_samplemaskin gl_sampleposition gl_tesscoord
-              gl_tesslevelinner gl_tesslevelouter gl_vertexid gl_viewportindex
-              gl_workgroupid gl_workgroupsize greaterthan greaterthanequal
-              groupmemorybarrier imageatomicadd imageatomicand
-              imageatomiccompswap imageatomicexchange imageatomicmax
-              imageatomicmin imageatomicor imageatomicxor imageload
-              imagesamples imagesize imagestore imulextended intbitstofloat
-              interpolateatcentroid interpolateatoffset interpolateatsample
-              inverse inversesqrt isinf isnan ldexp length lessthan
-              lessthanequal log log2 matrixcompmult max memorybarrier
-              memorybarrieratomiccounter memorybarrierbuffer memorybarrierimage
-              memorybarriershared min mix mod modf noise noise1 noise2 noise3
-              noise4 normalize not notequal outerproduct packdouble2x32
-              packhalf2x16 packsnorm2x16 packsnorm4x8 packunorm packunorm2x16
-              packunorm4x8 pow radians reflect refract round roundeven sign sin
-              sinh smoothstep sqrt step tan tanh texelfetch texelfetchoffset
-              texture texturegather texturegatheroffset texturegatheroffsets
-              texturegrad texturegradoffset texturelod texturelodoffset
-              textureoffset textureproj textureprojgrad textureprojgradoffset
-              textureprojlod textureprojlodoffset textureprojoffset
-              texturequerylevels texturequerylod texturesamples texturesize
-              transpose trunc uaddcarry uintbitstofloat umulextended
-              unpackdouble2x32 unpackhalf2x16 unpacksnorm2x16 unpacksnorm4x8
-              unpackunorm unpackunorm2x16 unpackunorm4x8 usubborrow
-              ;;;; Type constructors.
-              ;;;; https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)#Basic_types
-              ;; > Vectors
-              ;; > Each of the scalar types, including booleans, have 2, 3, and 4-component vector equivalents. The n digit below can be 2, 3, or 4:
-              bvec2 bvec3 bvec4 ivec2 ivec3 ivec4 uvec2 uvec3 uvec4 vec2 vec3
-              vec4 dvec2 dvec3 dvec4
-              ;; > Matrices
-              ;; > In addition to vectors, there are also matrix types. All matrix types are floating-point, either single-precision or double-precision. Matrix types are as follows, where n and m can be the numbers 2, 3, or 4:
-              mat2x2 mat2x3 mat2x4 mat3x2 mat3x3 mat3x4 mat4x2 mat4x3 mat4x4
-              mat2 mat3 mat4
-              ;;;; Operator
-              * / + - < > <= >= == && ^^ |\|\|| % << >> & ^ |\||))))
+    '(;;;; Built in functions.
+      ;;;; https://www.khronos.org/registry/OpenGL-Refpages/gl4/index.php
+      "abs" "acos" "acosh" "all" "any" "asin" "asinh" "atan" "atanh"
+      "atomicAdd" "atomicAnd" "atomicCompSwap" "atomicCounter"
+      "atomicCounterDecrement" "atomicCounterIncrement" "atomicExchange"
+      "atomicMax" "atomicMin" "atomicOr" "atomicXor" "barrier" "bitCount"
+      "bitfieldExtract" "bitfieldInsert" "bitfieldReverse" "ceil" "clamp" "cos"
+      "cosh" "cross" "degrees" "determinant" "dFdx" "dFdxCoarse" "dFdxFine"
+      "dFdy" "dFdyCoarse" "dFdyFine" "distance" "dot" "EmitStreamVertex"
+      "EmitVertex" "EndPrimitive" "EndStreamPrimitive" "equal" "exp" "exp2"
+      "faceforward" "findLSB" "findMSB" "floatBitsToInt" "floatBitsToUint"
+      "floor" "fma" "fract" "frexp" "fwidth" "fwidthCoarse" "fwidthFine"
+      "gl_ClipDistance" "gl_CullDistance" "gl_FragCoord" "gl_FragDepth"
+      "gl_FrontFacing" "gl_GlobalInvocationID" "gl_HelperInvocation"
+      "gl_InstanceID" "gl_InvocationID" "gl_Layer" "gl_LocalInvocationID"
+      "gl_LocalInvocationIndex" "gl_NumSamples" "gl_NumWorkGroups"
+      "gl_PatchVerticesIn" "gl_PointCoord" "gl_PointSize" "gl_Position"
+      "gl_PrimitiveID" "gl_PrimitiveIDIn" "gl_SampleID" "gl_SampleMask"
+      "gl_SampleMaskIn" "gl_SamplePosition" "gl_TessCoord" "gl_TessLevelInner"
+      "gl_TessLevelOuter" "gl_VertexID" "gl_ViewportIndex" "gl_WorkGroupID"
+      "gl_WorkGroupSize" "greaterThan" "greaterThanEqual" "groupMemoryBarrier"
+      "imageAtomicAdd" "imageAtomicAnd" "imageAtomicCompSwap"
+      "imageAtomicExchange" "imageAtomicMax" "imageAtomicMin" "imageAtomicOr"
+      "imageAtomicXor" "imageLoad" "imageSamples" "imageSize" "imageStore"
+      "imulExtended" "intBitsToFloat" "interpolateAtCentroid"
+      "interpolateAtOffset" "interpolateAtSample" "inverse" "inversesqrt"
+      "isinf" "isnan" "ldexp" "length" "lessThan" "lessThanEqual" "log" "log2"
+      "matrixCompMult" "max" "memoryBarrier" "memoryBarrierAtomicCounter"
+      "memoryBarrierBuffer" "memoryBarrierImage" "memoryBarrierShared" "min"
+      "mix" "mod" "modf" "noise" "noise1" "noise2" "noise3" "noise4"
+      "normalize" "not" "notEqual" "outerProduct" "packDouble2x32"
+      "packHalf2x16" "packSnorm2x16" "packSnorm4x8" "packUnorm" "packUnorm2x16"
+      "packUnorm4x8" "pow" "radians" "reflect" "refract" "round" "roundEven"
+      "sign" "sin" "sinh" "smoothstep" "sqrt" "step" "tan" "tanh" "texelFetch"
+      "texelFetchOffset" "texture" "textureGather" "textureGatherOffset"
+      "textureGatherOffsets" "textureGrad" "textureGradOffset" "textureLod"
+      "textureLodOffset" "textureOffset" "textureProj" "textureProjGrad"
+      "textureProjGradOffset" "textureProjLod" "textureProjLodOffset"
+      "textureProjOffset" "textureQueryLevels" "textureQueryLod"
+      "textureSamples" "textureSize" "transpose" "trunc" "uaddCarry"
+      "uintBitsToFloat" "umulExtended" "unpackDouble2x32" "unpackHalf2x16"
+      "unpackSnorm2x16" "unpackSnorm4x8" "unpackUnorm" "unpackUnorm2x16"
+      "unpackUnorm4x8" "usubBorrow"
+      ;;;; Type constructors.
+      ;;;; https://www.khronos.org/opengl/wiki/Data_Type_(GLSL)#Basic_types
+      ;; > Vectors
+      ;; > Each of the scalar types, including booleans, have 2, 3, and 4-component vector equivalents. The n digit below can be 2, 3, or 4:
+      "bvec2" "bvec3" "bvec4" "ivec2" "ivec3" "ivec4" "uvec2" "uvec3" "uvec4"
+      "vec2" "vec3" "vec4" "dvec2" "dvec3" "dvec4"
+      ;; > Matrices
+      ;; > In addition to vectors, there are also matrix types. All matrix types are floating-point, either single-precision or double-precision. Matrix types are as follows, where n and m can be the numbers 2, 3, or 4:
+      "mat2x2" "mat2x3" "mat2x4" "mat3x2" "mat3x3" "mat3x4" "mat4x2" "mat4x3"
+      "mat4x4" "mat2" "mat3" "mat4"
+      ;;;; Operator
+      "*" "/" "+" "-" "<" ">" "<=" ">=" "==" "&&" "^^" "||" "%" "<<" ">>" "&"
+      "^" "|")))
 
 (defvar *shader-vars*)
 
@@ -122,7 +124,7 @@
 
 (defun glsl-funcall (stream exp)
   (setf stream (or stream *standard-output*))
-  (assert (gethash (symbol-name (car exp)) *glsl-functions*) ()
+  (assert (gethash (symbol-camel-case (car exp)) *glsl-functions*) ()
     'unknown-glsl-function :name (car exp))
   (funcall (formatter "~/fude-gl:glsl-symbol/~:<~@{~W~^, ~@_~}~:>") stream
            (car exp) (cdr exp)))
